2fe6cf8c88139cd7c4da2f605554731331e52303
John <jkrouli@list.ru>
2023-05-01 14:59:44 +0300

    Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <=999)
+{
+    int lastNumber = number % 10;
+    Console.Write($"Последняя цифра числа- {lastNumber}");
+} 
+else Console.WriteLine ("Ошибка ввода! Введите трехзначное число.");
