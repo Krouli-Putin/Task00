229c55276b8f0911e425baca4ab1269657186b26
John <jkrouli@list.ru>
2023-05-02 13:34:39 +0300

    Task005ДЗ

---
+++ Task005DZ/Program.cs
@@ -0,0 +1,19 @@
+﻿// 1.Напишите программу которая на вход принмает два числа
+// 2. И выдает на вход какое число больше, а какое мешьше
+Console.WriteLine("Введите первое число!");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int square = Convert.ToInt32(Console.ReadLine());
+if (number > square && number < square && number == square)
+{
+    Console.WriteLine("большее число {number}, меньшее число: {square}");
+}
+ else
+ {
+    Console.WriteLine($"Большее число {number}, меньшее число {square}");
+ }
+
+
+
+
+
