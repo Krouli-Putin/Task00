9cda98edbf7751123d8cf97071b894b56aa9896b
John <jkrouli@list.ru>
2023-05-03 14:15:15 +0300

    Task006ДЗ

--- Task005DZ/Program.cs
+++ Task005DZ/Program.cs
@@ -1,17 +1,14 @@
 ﻿// 1.Напишите программу которая на вход принмает два числа
 // 2. И выдает на вход какое число больше, а какое мешьше
-Console.WriteLine("Введите первое число!");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int square = Convert.ToInt32(Console.ReadLine());
-if (number > square && number < square && number == square)
+Console.WriteLine("Введите первое число!"); 
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите второе число"); 
+int b = Convert.ToInt32(Console.ReadLine()); 
+ if (a > b)
 {
-    Console.WriteLine("большее число {number}, меньшее число: {square}");
+Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", a, b);
 }
- else
- {
-    Console.WriteLine($"Большее число {number}, меньшее число {square}");
- }
+else Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", b, a);
 
 
 
---
+++ Task006DZ/Program.cs
@@ -0,0 +1,12 @@
+﻿// Код, который принимает на вход 3 числа 
+// На выходе выдает наибольшее (Вроде ничем не отличается от 1 задания)
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+Console.WriteLine($"Максимальное из этих чисел = {max}");
