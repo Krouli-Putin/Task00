53ded53221e732385d5c5597307119f54e50fdd8
John <jkrouli@list.ru>
2023-05-03 14:22:49 +0300

    Task007ДЗ

---
+++ Task007DZ/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine($"Число: {a} четное");
+}
+else System.Console.WriteLine($"Число: {a} нечетное");
